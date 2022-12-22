a6fbb5beea8bc2293bed42c54378e7829145c820
Brian Feucht <brianfeucht@gmail.com>
2022-12-22 14:30:50 -0800

    add script to update from latest github build if possible

---
+++ src/setup/update-app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+release_id=$(curl -s "https://api.github.com/repos/brianfeucht/ha-flutter-pi/releases/latest" | jq '.assets[] | select(.name == "remote_flutter_app.tar.gz") | .id')
+
+
+if [ ! -d /home/pi/releases/$release_id ]; then
+  mkdir -p /home/pi/releases/$release_id;
+
+  wget -O /tmp/$release_id-remote_flutter_app.tar.gz https://github.com/brianfeucht/ha-flutter-pi/releases/download/latest/remote_flutter_app.tar.gz
+  tar -xvf /tmp/$release_id-remote_flutter_app.tar.gz -C /home/pi/releases/$release_id
+  rm /tmp/$release_id-remote_flutter_app.tar.gz
+
+  ls -dt /home/pi/releases/*/ | tail -n +11 | xargs rm -rf
+fi
+
+current_release=$(readlink -f /home/pi/current_release)
+
+if [ ! $current_release -ef '/home/pi/releases/$release_id' ]; then
+  ln -sf /home/pi/releases/$release_id /home/pi/current_release
+  pkill flutter-pi
+fi
