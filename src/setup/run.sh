f5582e29475ae7df1d4a6c57ddf425619584c287
Brian Feucht <brianfeucht@gmail.com>
2022-12-22 15:39:47 -0800

    Fix run script bug.  Look at correct path in symlink

--- src/setup/run.sh
+++ src/setup/run.sh
@@ -3,7 +3,7 @@
 if tty | grep -q '/dev/pts' ; then
  echo "SSH Terminal detected"
 else
- until flutter-pi --release -d "190,190" release; do
+ until flutter-pi --release -d "190,190" ~/current_release/build/flutter_assets; do
     echo "Server 'myserver' crashed with exit code $?.  Respawning.." >&2
     sleep 1
  done
