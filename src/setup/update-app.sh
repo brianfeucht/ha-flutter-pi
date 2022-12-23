fa1efc290ccfabb71a51b5d49572d8f806185d85
Brian Feucht <brianfeucht@gmail.com>
2022-12-22 16:03:12 -0800

    Add some log messaging and fix bug in updater

--- src/setup/update-app.sh
+++ src/setup/update-app.sh
@@ -2,8 +2,10 @@
 
 release_id=$(curl -s "https://api.github.com/repos/brianfeucht/ha-flutter-pi/releases/latest" | jq '.assets[] | select(.name == "remote_flutter_app.tar.gz") | .id')
 
+echo "Latest release is $release_id";
 
 if [ ! -d /home/pi/releases/$release_id ]; then
+  echo "Downloading latest release";
   mkdir -p /home/pi/releases/$release_id;
 
   wget -O /tmp/$release_id-remote_flutter_app.tar.gz https://github.com/brianfeucht/ha-flutter-pi/releases/download/latest/remote_flutter_app.tar.gz
@@ -16,6 +18,7 @@ fi
 current_release=$(readlink -f /home/pi/current_release)
 
 if [ ! $current_release -ef '/home/pi/releases/$release_id' ]; then
-  ln -sf /home/pi/releases/$release_id /home/pi/current_release
+  echo "Updating current_release symlink and restarting app";
+  ln -sfn /home/pi/releases/$release_id /home/pi/current_release
   pkill flutter-pi
 fi
