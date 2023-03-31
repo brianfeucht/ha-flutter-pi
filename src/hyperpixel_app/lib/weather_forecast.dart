cf4c551ede43f4e0472905715ce8baeda9e6325e
Brian Feucht <brianfeucht@gmail.com>
2023-03-31 09:18:45 -0700

    Switch forecast to restartable timer.  See if this fixes no updates issue

--- src/hyperpixel_app/lib/weather_forecast.dart
+++ src/hyperpixel_app/lib/weather_forecast.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unnecessary_brace_in_string_interps
 import 'dart:async';
 import 'dart:convert';
+import 'package:async/async.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:remote_flutter_app/models/weather_forecast.dart';
@@ -11,14 +12,16 @@ class WeatherForecast {
   final WeatherForecastModel _currentForecast = WeatherForecastModel();
   final AppLog _appLog;
 
-  late Timer updateForecastTimer = Timer.periodic(
-      const Duration(minutes: 30), (_) => retriveLatestForecast());
+  late RestartableTimer _updateForecastTimer;
 
   // Contains a auth token.  I don't really care, but lets not make it obvious
   static String parkdaleForecastUrl = utf8.decode(base64.decode(
       "aHR0cHM6Ly9zd2Qud2VhdGhlcmZsb3cuY29tL3N3ZC9yZXN0L2JldHRlcl9mb3JlY2FzdD9zdGF0aW9uX2lkPTI2MTgzJnVuaXRzX3RlbXA9ZiZ1bml0c193aW5kPW1waCZ1bml0c19wcmVzc3VyZT1tYiZ1bml0c19wcmVjaXA9aW4mdW5pdHNfZGlzdGFuY2U9bWkmYXBpX2tleT1iNGM3YjhlZS02ODYwLTQwMWItYTI0ZS1hZjEyOWU5ZmI4MDY="));
 
-  WeatherForecast(AppLog appLog) : _appLog = appLog;
+  WeatherForecast(AppLog appLog) : _appLog = appLog {
+    _updateForecastTimer = RestartableTimer(
+        const Duration(minutes: 30), () => retriveLatestForecast());
+  }
 
   WeatherForecastModel get currentForecast {
     return _currentForecast;
@@ -44,6 +47,8 @@ class WeatherForecast {
       _currentForecast.setDailyForecasts = dailyForecasts;
     } catch (e) {
       _appLog.addLog("Forecast retrieval failed:\n ${e}");
+    } finally {
+      _updateForecastTimer.reset();
     }
   }
 }
